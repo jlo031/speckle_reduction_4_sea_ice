72a9acc7fc271227bcd0d92aae945feddfae39d6
Johannes Lohse <johannes.p.lohse@uit.no>
2025-03-05 16:53:33 +0100

    updated classification

--- classify_images.py
+++ classify_images.py
@@ -10,16 +10,16 @@ import shutil
 
 from loguru import logger
 
-import ice_type_classification.classification as cl
+import GLIA_classifier.classification as glass
+
+from config.folder_structure import *
 
 # -------------------------------------------------------------------------- #
 # -------------------------------------------------------------------------- #
 
-clf_model_path = pathlib.Path('/home/jo/work/ice_type_classification/src/ice_type_classification/clf_models/belgica_bank_ice_types.pickle')
+clf_model_path = pathlib.Path('config/belgica_bank_ice_types_2022.pickle').resolve()
 
-subfolder_list = [ 'ML_1x1', 'ML_9x9', 'ML_21x21', 'MuLoG', 'SARBM3D', 'ines', 'denoised']
-
-from folder_structure import *
+procesing_methods = [ 'ML_1x1', 'ML_9x9', 'ML_21x21', 'MuLoG', 'SARBM3D', 'baseline', 'proposed']
 
 # -------------------------------------------------------------------------- #
 # -------------------------------------------------------------------------- #
@@ -35,13 +35,13 @@ for S1_name in S1_list:
     logger.info(f'Processing S1 image: {S1_name}')
 
     # loop through all processing methods
-    for subfolder in subfolder_list:
+    for procesing_method in procesing_methods:
 
-        logger.info(f'Processing speckle reduction: {subfolder}')
+        logger.info(f'Processing speckle reduction: {procesing_method}')
  
         # build paths to feature and results folder
-        feat_folder   = S1_DIR / 'features' / f'{subfolder}' / f'{S1_name}'
-        result_folder = S1_RESULT_DIR / f'{subfolder}'
+        feat_folder   = S1_DIR / 'features' / f'{procesing_method}' / f'{S1_name}'
+        result_folder = S1_RESULT_DIR / f'{procesing_method}'
 
         if not feat_folder.is_dir():
             logger.error(f'Current feature folder does not exists: {feat_folder}')
@@ -55,26 +55,30 @@ for S1_name in S1_list:
         logger.debug(f'feature_list:  {feature_list}')
 
         # copy IA images into feature folder if needed
-        if subfolder in ['MuLoG', 'SARBM3D', 'ines', 'denoised'] and 'IA.img' not in feature_list:
+        if procesing_method in ['MuLoG', 'SARBM3D', 'baseline', 'proposed'] and 'IA.img' not in feature_list:
             logger.info('Copying IA image into local feature folder')
             shutil.copyfile(S1_DIR/'features'/'ML_1x1'/f'{S1_name}'/'IA.img' , feat_folder/'IA.img')
             shutil.copyfile(S1_DIR/'features'/'ML_1x1'/f'{S1_name}'/'IA.hdr' , feat_folder/'IA.hdr')
 
-        cl.classify_S1_image_from_feature_folder(
-            feat_folder.as_posix(),
-            result_folder.as_posix(),
-            clf_model_path.as_posix(),
-            uncertainties = False,
-            valid_mask = False,
-            block_size = 1000000.0,
+        # copy valid_mask into feature folder if needed
+        if 'valid' not in feature_list:
+            logger.info('Copying valid mask image into local feature folder')
+            shutil.copyfile(S1_DIR / 'features' / f'{S1_name}' / 'valid.img' , feat_folder/'valid.img')
+            shutil.copyfile(S1_DIR / 'features' / f'{S1_name}' / 'valid.hdr' , feat_folder/'valid.hdr')
+
+# ------------------------------------------- #
+
+        glass.classify_image_from_feature_folder(
+            feat_folder,
+            result_folder,
+            clf_model_path,
+            use_valid_mask = True,
+            estimate_uncertainties = False,
+            uncertainty_params_dict = [],
             overwrite = False,
             loglevel = 'INFO',
         )
 
-
-# -------------------------------------------------------------------------- #
-# -------------------------------------------------------------------------- #
-# -------------------------------------------------------------------------- #
 # -------------------------------------------------------------------------- #
 # -------------------------------------------------------------------------- #
 
